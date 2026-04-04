916ac9d5a883a66deff2d7481eef456d3d94fa32
zjp <jiping_zhou@foxmail.com>
2026-04-04 17:13:46 +0000

    gdb: fix docker

--- tool/run_docker.sh
+++ tool/run_docker.sh
@@ -3,5 +3,11 @@
 set -eoux pipefail
 
 podman build -t gdb tool/gdb
-podman run -d -v .:/KMiri --replace --name kmiri localhost/gdb sleep infinity
+
+# Map current local directory to /KMiri in the container.
+# Allow to disable ASLR (address space randomization) by GDB.
+podman run -d -v .:/KMiri \
+  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+  --replace --name kmiri localhost/gdb sleep infinity
+
 podman exec -it kmiri fish
