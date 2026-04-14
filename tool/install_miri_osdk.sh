74a6b7cf306149afcc4c24203f6a83de33fe9fc0
zjp <jiping_zhou@foxmail.com>
2026-04-14 02:52:00 +0000

    install_miri_osdk: install and run osdk with OSDK_LOCAL_DEV=1

--- tool/install_miri_osdk.sh
+++ tool/install_miri_osdk.sh
@@ -7,5 +7,12 @@ cd /KMiri/kmiri
 cargo clean
 ./miri install --debug --features=tracing
 
+# Install osdk.
 cd /KMiri/asterinas
-make install_osdk
+cargo clean
+OSDK_LOCAL_DEV=1 make install_osdk
+
+# Test if gdb works.
+cd /KMiri/tests/os
+OSDK_LOCAL_DEV=1 cargo osdk miri test
+OSDK_LOCAL_DEV=1 rust-gdb -x miri.gdb --args cargo osdk miri test
