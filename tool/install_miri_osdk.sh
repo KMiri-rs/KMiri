e6a5dc94092ae6f57cf4745fec6d7bca353193d9
zjp <jiping_zhou@foxmail.com>
2026-05-08 09:04:10 +0800

    install_miri_osdk: remove tracing feature when compiling miri
    make the script usable in host system

--- tool/install_miri_osdk.sh
+++ tool/install_miri_osdk.sh
@@ -2,17 +2,19 @@
 
 set -eoux pipefail
 
+CWD=${CWD:=$PWD}
+
 # Install miri.
-cd /KMiri/kmiri
-cargo clean
-./miri install --debug --features=tracing
+cd "${CWD}"/kmiri
+# cargo clean
+./miri install --debug
 
 # Install osdk.
-cd /KMiri/asterinas
-cargo clean
+cd "${CWD}"/asterinas
+# cargo clean
 OSDK_LOCAL_DEV=1 make install_osdk
 
 # Test if gdb works.
-cd /KMiri/tests/os
-OSDK_LOCAL_DEV=1 cargo osdk miri test
-OSDK_LOCAL_DEV=1 rust-gdb -x miri.gdb --args cargo osdk miri test
+# cd "${CWD}"/tests/os
+# OSDK_LOCAL_DEV=1 cargo osdk miri test
+# OSDK_LOCAL_DEV=1 rust-gdb -x miri.gdb --args cargo osdk miri test
