6a30c46339cffadc939cd04a40049c685c385d97
zjp <jiping_zhou@foxmail.com>
2026-08-01 18:36:41 +0800

    test: schedule task via yield_now
    fix https://github.com/KMiri-rs/KMiri/issues/106
    
    Each task needs explicit yield_now to push back current thread in run queue
    and switch to the new task.
    
    miri_main task schedule_info=TaskScheduleInfo { cpu: AtomicCpuId(0) }
    main: before yield_now
    [switch_to_task] next_task_addr=0xffff800002116f10
    [kernel_task_entry]
    [current_task] &CURRENT_TASK_PTR=0xffffffff80400558
    current_task=0xffff800002116f10
    A custom task from miri_main!
    main: after yield_now

--- tests/init/src/lib.rs
+++ tests/init/src/lib.rs
@@ -1,18 +1,19 @@
 #![no_std]
 #![deny(unsafe_code)]
-#![feature(format_args_nl)]
-#![feature(stmt_expr_attributes)]
+#![feature(format_args_nl, stmt_expr_attributes)]
 
 extern crate alloc;
-use ostd::prelude::*;
 
 #[ostd::ktest::miri_main]
 fn kernel_main() {
-    println!("Hello world from guest kernel!");
+    ostd::miri_println!("Hello world from guest kernel!");
 
     let task =
         ostd::task::TaskOptions::new(|| ostd::miri_println!("A custom task from miri_main!"))
             .spawn()
             .unwrap();
     ostd::miri_println!("miri_main task schedule_info={:?}", task.schedule_info());
+    ostd::miri_println!("main: before yield_now");
+    ostd::task::Task::yield_now();
+    ostd::miri_println!("main: after yield_now");
 }
