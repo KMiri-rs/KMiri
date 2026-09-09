383cfb85ea7d6a7d3029397a49cde2eb508f72f8
zjp <jiping_zhou@foxmail.com>
2026-09-09 23:45:41 +0800

    kmiri-helper: replace `tcx.crate_hash` by `tcx.stable_crate_id`
    to fix panic due to `crate_hash` relies on stable hash on HIR owners
    and some flags to open
    
    close https://github.com/KMiri-rs/KMiri/issues/178

--- kmiri-helper/src/bin/kmiri-helper/main.rs
+++ kmiri-helper/src/bin/kmiri-helper/main.rs
@@ -44,8 +44,15 @@ fn analysis(tcx: TyCtxt) -> ControlFlow<()> {
 
     let dir_target = std::env::var(kmiri_helper::ENV__KMIRI_DIR_TARGET).unwrap();
     let dir_analysis = Path::new(&dir_target).join(kmiri_helper::ANALYSIS);
-    let crate_hash = tcx.crate_hash(internal(tcx, krate.id));
-    let json_path = dir_analysis.join(format!("{}_{crate_hash}.json", krate.name));
+
+    // Use stable_crate_id instead of tcx.crate_hash() to avoid triggering HIR hash computation.
+    // tcx.crate_hash() requires calling opt_hash.unwrap() on OwnerInfo, which panics when needs_hir_hash() is false.
+    // Instead we use StableCrateId which can be obtained without accessing HIR owners.
+    let stable_id = tcx.stable_crate_id(internal(tcx, krate.id)).as_u64();
+    let json_path = dir_analysis.join(format!("{}_{stable_id}.json", krate.name,));
+    if let Err(err) = std::fs::create_dir_all(&dir_analysis) {
+        panic!("Failed to create analysis directory {dir_analysis:?}: {err:?}");
+    }
     let file = std::fs::File::create(dbg!(json_path)).unwrap();
     serde_json::to_writer_pretty(file, &v_fn).unwrap();
 
